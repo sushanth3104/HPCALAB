4633454dba3312d0dd8a61e8e9a054b55fac6cb9
sushanth3104 <sushkumarthaviti@gmail.com>
2025-04-01 23:00:20 +0530

    ADD CONTROL LOGIC

--- SingleCycleCPU/Control.v
+++ SingleCycleCPU/Control.v
@@ -1,16 +1,153 @@
 module Control (
     input [6:0] opcode,
     output  branch,
+    output jump,
     output  memRead,
-    output  memtoReg,
+    output  [1:0]ResultSrc,
     output  [1:0] ALUOp,
+    output PCLoad,
     output  memWrite,
     output  ALUSrc,
     output  regWrite,
     );
 
-    // TODO: implement your Control here
-    // Hint: follow the Architecture to set output signal
+
+
+// For ALUOp
+parameter Load_Store_Type = 0;
+parameter Branch_Type = 1;
+parameter IR_Type = 2;
+
+
+
+
+// Opcodes
+parameter LoadInst = 3;
+parameter StoreInst = 35;
+parameter BranchInst = 99;
+parameter I_TypeInst = 19;
+parameter R_TypeInst = 51;
+parameter J_TypeInst = 111;
+parameter JALR_Inst = 103;
+
+
+always@(*)begin
+
+    case(opcode)
+
+        LoadInst:begin
+
+            
+            branch = 0;
+            jump = 0;
+            memRead = 1;
+            ResultSrc = 2'b01;
+            ALUOp = Load_Store_Type;
+            PCLoad = 0;
+            memWrite = 0;
+            ALUSrc = 1;
+            regWrite = 1;
+            
+            
+
+        end
+
+        StoreInst:begin
+            
+            
+            branch = 0;
+            jump = 0;
+            memRead = 0;
+            ResultSrc = 2'b00;
+            ALUOp = Load_Store_Type;
+            PCLoad = 0;
+            memWrite = 1;
+            ALUSrc = 1;
+            regWrite = 0;
+
+        end
+
+        BranchInst:begin
+
+            
+            branch = 1;
+            jump = 0;
+            memRead = 0;
+            ResultSrc = 2'b00;
+            ALUOp = Branch_Type;
+            PCLoad = 0;
+            memWrite = 0;
+            ALUSrc = 0;
+            regWrite = 0;
+
+        end
+
+        I_TypeInst:begin
+
+            
+            branch = 0;
+            jump = 0;
+            memRead = 0;
+            ResultSrc = 2'b00;
+            ALUOp = IR_Type;
+            PCLoad
+            memWrite = 0;
+            ALUSrc = 1;
+            regWrite = 1;
+
+        end
+
+        R_TypeInst:begin
+
+            
+            branch = 0;
+            jump = 0;
+            memRead = 0;
+            ResultSrc = 2'b00;
+            ALUOp = IR_Type;
+            PCLoad = 0;
+            memWrite = 0;
+            ALUSrc = 0;
+            regWrite = 1;
+
+        end
+
+        J_TypeInst:begin
+
+            
+            branch = 0;
+            jump = 1;
+            memRead = 0;
+            ResultSrc = 2'b10;
+            ALUOp = 0;
+            PCLoad = 0;
+            memWrite = 0;
+            ALUSrc = 0;
+            regWrite = 1;
+
+        end
+
+        JALR_Inst:begin
+
+            
+            branch = 0;
+            jump = 1;
+            memRead = 0;
+            ResultSrc = 2'b10;
+            ALUOp = Load_Store_Type; // For addition
+            PCLoad = 1;
+            memWrite = 0;
+            ALUSrc = 1;
+            regWrite = 1;
+
+        end
+
+        default : {ResultSrc,ALUOp,branch,jump,memRead,PCLoad,memWrite,ALUSrc,regWrite} = 0;
+
+    endcase
+
+end
+
 
 endmodule
 
